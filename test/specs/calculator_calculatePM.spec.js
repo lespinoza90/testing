5cb9f4a61ab839ba734089149206b1d536b5c38c
unknown <luis.espinoza@acklenavenue.com>
2019-12-02 13:47:00 -0600

    testing hammer

--- test/specs/calculator_calculatePM.spec.js
+++ test/specs/calculator_calculatePM.spec.js
@@ -1,5 +1,4 @@
 const expect= require('chai').expect;
-//
 const CalculatorPage = require('../../calculator.pageobjects');
 const LotContent= require ('../../helpers/testdata').LotContent;
 const LotContentValues= require ('../../helpers/testdata').LotContentValues;
